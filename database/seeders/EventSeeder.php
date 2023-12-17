3333d51fd37b920049cef09696cb3f4de39f5faa
BryanSamuelIan <bryansamuelangka@gmail.com>
2023-12-18 00:21:35 +0700

    .

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Event::create([
+            'name' => 'tes 1',
+            'banner' => 'images/tes1.png',
+            'is_active' => true
+        ]);
+        Event::create([
+            'name' => 'tes 2',
+            'banner' => 'images/tes2.jpg',
+            'is_active' => true
+        ]);
     }
 }
