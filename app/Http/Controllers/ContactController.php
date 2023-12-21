dcdc16ac3896c7b6b6ac86b343ccdc6cd14a1068
BryanSamuelIan <bryansamuelangka@gmail.com>
2023-12-21 15:15:52 +0700

    bug..

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+use Symfony\Component\Mime\Part\TextPart;
 
 class ContactController extends Controller
 {
@@ -14,19 +15,27 @@ class ContactController extends Controller
         $messageContent = $request->input('message');
         $subject = $request->input('subject');
 
+
         try {
-            // Sending email using Laravel Mail class
-            Mail::raw("Name: $name\nEmail: $email\nMessage: $messageContent", function ($message) use ($subject) {
-                $message->to('bryansamuelangka@gmail.com')->subject("New Feedback: $subject");
-            });
+            Mail::raw(
+                "Name: $name\nEmail: $email\nSubject: $subject\nMessage Content:\n$messageContent",
+                function ($message) use ($subject) {
+                    $message->to('bryansamuelangka@gmail.com')->subject($subject);
+                }
+            );
 
-            $status = "sent";
 
-            // Returning view after sending email with the status
-            return view('contact', ['status' => $status, 'pagetitle' => 'Contact']);
+            // Return view after sending email with the status
+
+            return view('contact', ['status' => true, 'pagetitle' => 'Contact']);
 
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            // Log the error for debugging purposes
+            \Log::error('Failed to send email: ' . $e->getMessage());
+
+            // Return an error view
+            $status = "failed";
+            return view('contact', ['status' => false, 'pagetitle' => 'Contact']);
         }
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,26 +9,37 @@
                     <p class="mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Punya masalah?
                         Mau beri saran? Beri tahu kami.</p>
 
-                        <form action="{{ route('sendEmail') }}" method="post" class="space-y-2">
+                    <form action="{{ route('sendEmail') }}" method="POST" class="space-y-2">
                         @csrf
-                        <div>
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email anda</label>
-                            <input type="email" id="email"
-                                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                placeholder="Contoh: onoeikipacet@gmail.com" required>
+                        <div class="grid md:grid-cols-2 md:gap-x-4 ">
+                            <div>
+                                <label for="name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nama
+                                    anda</label>
+                                <input type="text" id="name" name="name"
+                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                    placeholder="Isikan nama Anda..." required>
+                            </div>
+                            <div>
+                                <label for="email"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email
+                                    anda</label>
+                                <input type="email" id="email" name="email"
+                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                    placeholder="Contoh: onoeikipacet@gmail.com" required>
+                            </div>
                         </div>
                         <div>
                             <label for="subject"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subyek</label>
-                            <input type="text" id="subject"
+                            <input type="text" id="subject" name="subject"
                                 class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                 placeholder="Hal apa yang bisa kami bantu?" required>
                         </div>
                         <div class="sm:col-span-2">
                             <label for="message"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Pesan anda</label>
-                            <textarea id="message" rows="6"
+                            <textarea id="message" rows="6" name="message"
                                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="Tuliskan komentar Anda..."></textarea>
                         </div>
@@ -37,9 +48,9 @@
                             message</button>
 
                     </form>
-                    @if(isset($status) && $status == 'sent')
-                    <p class="text-green-500 font-semibold text-center mb-4">Message Sent!</p>
-                @endif
+                    @if (isset($status))
+                        <p class="text-green-500 font-semibold text-center mb-4">Message Sent!</p>
+                    @endif
                 </div>
             </div>
             <iframe
